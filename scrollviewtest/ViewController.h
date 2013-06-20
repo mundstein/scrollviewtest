28ab9fd0ce2a42d5edf53c517f0e18dab267312c
Sascha Mundstein <mundstein@gmail.com>
2013-06-20 16:21:02 +0200

    Added gitignore. Basic View Controllers.

--- scrollviewtest/ViewController.h
+++ scrollviewtest/ViewController.h
@@ -10,4 +10,8 @@
 
 @interface ViewController : UIViewController
 
+@property (nonatomic, strong) UIScrollView *scrollView;
+@property (nonatomic, strong) UIScrollView *verticalScrollView;
+
+
 @end
--- scrollviewtest/ViewController.m
+++ scrollviewtest/ViewController.m
@@ -17,7 +17,28 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	UIScrollView *sv = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    sv.pagingEnabled = YES;
+    self.scrollView = sv;
+    
+    self.scrollView.contentSize = CGSizeMake(sv.frame.size.width*2, sv.frame.size.height);
+    
+    for (int i = 0; i< 2; i++) {
+        CGRect svFrame= self.view.bounds;
+        svFrame.origin.x = i*self.scrollView.bounds.size.width;
+        UIScrollView *sv2 = [[UIScrollView alloc] initWithFrame:svFrame];
+        sv2.pagingEnabled = YES;
+        sv2.contentSize = CGSizeMake(svFrame.size.width, svFrame.size.height*2);
+        [self.scrollView addSubview:sv2];
+        sv2.backgroundColor = i==0 ? UIColor.redColor : UIColor.orangeColor;
+        for (int j=0; j<2; j++) {
+            UIView *v = [[UIView alloc] initWithFrame:CGRectMake(0, sv.bounds.size.height*j, sv2.bounds.size.width, sv2.bounds.size.height)];
+            [sv2 addSubview:v];
+            v.backgroundColor = j==0 ? (i==0 ? UIColor.yellowColor : UIColor.magentaColor) : UIColor.clearColor;
+        }
+    }
+    
+    [self.view addSubview:sv];
 }
 
 - (void)didReceiveMemoryWarning
